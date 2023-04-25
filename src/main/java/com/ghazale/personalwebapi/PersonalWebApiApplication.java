2faf4f95472422f6dbe2949ee7df19c28bd05417
Ghazaleh Rashidi <Gh.Rashidi@sadad.co.ir>
2023-04-25 14:31:55 +0330

    initial commit

---
+++ src/main/java/com/ghazale/personalwebapi/PersonalWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.ghazale.personalwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghazale/personalwebapi/PersonalWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghazale.personalwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
